7773b324cc4abdf7663fee52d9dc4cd248127116
Utomo Hendra Saputra <utomo.hendra.saputra@gmail.com>
2018-10-18 21:52:49 +0700

    update mvc

--- controllers/employee_controller.js
+++ controllers/employee_controller.js
@@ -9,6 +9,27 @@ const getEmployee = (request, respond) => {
   })
 }
 
+const insertEmployee = (req, res) => {
+  const { name } = req.body
+  console.log(`Accessing POST Methods in /employee`)
+  console.log(req.body)
+  
+  res.send(`Halo ${name}!`)
+}
+
+const updateEmployee = (req, res) => {
+  console.log(`Accessing PATCH Methods in /employee`)
+  res.send('0')
+}
+
+const removeEmployee = (req, res) => {
+  console.log(`Accessing DELETE Methods in /employee`)
+  res.send(`0`)
+}
+
 module.exports = {
   getEmployee,
+  insertEmployee,
+  updateEmployee,
+  removeEmployee,
 }
--- routes/route.js
+++ routes/route.js
@@ -26,20 +26,8 @@ router
 router
   .route(`/employee`)
   .get(employeeCtrl.getEmployee)
-  .post((req, res) => {
-    const { name } = req.body
-    console.log(`Accessing POST Methods in /employee`)
-    console.log(req.body)
-    
-    res.send(`Halo ${name}!`)
-  })
-  .patch((req, res) => {
-    console.log(`Accessing PATCH Methods in /employee`)
-    res.send('0')
-  })
-  .delete((req, res) => {
-    console.log(`Accessing DELETE Methods in /employee`)
-    res.send(`0`)
-  })
+  .post(employeeCtrl.insertEmployee)
+  .patch(employeeCtrl.updateEmployee)
+  .delete(employeeCtrl.removeEmployee)
 
 module.exports = router
